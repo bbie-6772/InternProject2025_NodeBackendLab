abf63a298976a74e11aefe15a9024aa020991720
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-04-18 19:16:44 +0900

    add: 클릭 핸들러 구현

--- eventServer/handlers/user/click.handler.js
+++ eventServer/handlers/user/click.handler.js
@@ -1,5 +1,15 @@
-
+import { config } from "../../../common/config/config.js";
+import { userSession } from "../../session.js"
+import { makePacket } from "../../utils/packet/makePacket.js";
 
 export const clickHandler = async (socket, payload ) => {
+    const user = userSession.getUserById(socket.id);
+    if(!user) {
+        const response = { error: "User not found" };
+        const packet = makePacket(config.header.packetType.S_ERROR_NOTIFICATION, response);
+        socket.write(packet);
+        return;
+    }
 
-}
+    user.addCount();
+}   
