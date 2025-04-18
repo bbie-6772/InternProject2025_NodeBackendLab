0b3c8226cf168519772bcbd54519bfff975d7db1
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-04-19 00:56:28 +0900

    fix: config 속성 사용

--- eventServer/utils/packet/makePacket.js
+++ eventServer/utils/packet/makePacket.js
@@ -1,11 +1,13 @@
+import { config } from "../../../common/config/config.js";
+
 export const makePacket = (packetType, payload) => {
     const payloadString = JSON.stringify(payload);
     const payloadBuffer = Buffer.from(payloadString, 'utf8');
 
-    const packetTypeBuffer = Buffer.alloc(2);
+    const packetTypeBuffer = Buffer.alloc(config.header.packetTypeByte);
     packetTypeBuffer.writeUInt16BE(packetType);
 
-    const payloadLengthBuffer = Buffer.alloc(4);
+    const payloadLengthBuffer = Buffer.alloc(config.header.payloadLengthByte);
     payloadLengthBuffer.writeUInt32BE(payloadBuffer.length);
 
     const packet = Buffer.concat([
