a06fa2b3265df162ff5a5d06f89789d15f590353
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-04-19 00:57:05 +0900

    add: 패킷 해석 함수 구현

---
+++ eventServer/utils/packet/parsePacket.js
@@ -0,0 +1,36 @@
+import { config } from "../../../common/config/config.js";
+
+export const parsePacket = (packet) => {
+    const packetTypeByte = config.header.packetTypeByte;
+    const payloadLengthByte = config.header.payloadLengthByte;
+    const defaultLength = packetTypeByte + payloadLengthByte
+    let payloadByte = 0;
+
+    try {
+        payloadByte = packet.readUInt32BE(packetTypeByte);
+    } catch (err) {
+        console.error("payload 읽기 실패", err);
+        return null;
+    }
+    
+    if (packet.length < defaultLength + payloadByte) {
+        console.error("패킷 길이 부족")
+        return null;
+    }
+
+    const packetType = packet.readUInt16BE(0);
+    const payloadBuffer = packet.subarray(defaultLength, defaultLength + payloadByte);
+    const payloadString = payloadBuffer.toString('utf8');
+    let payload;
+    try {
+        payload = JSON.parse(payloadString);
+    } catch (err) {
+        console.error("JSON 파싱 실패:", err);
+        return null;
+    }
+
+    return payload;
+}
+
+
+
