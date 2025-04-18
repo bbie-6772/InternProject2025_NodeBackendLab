8ee614b80883a2260a73ef8f305b92b317c942cf
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-04-19 03:10:52 +0900

    fix: DB 오류 상황 추가 구현

--- tests/unit/repositories/users.repository.spec.js
+++ tests/unit/repositories/users.repository.spec.js
@@ -2,6 +2,8 @@ import assert from 'assert';
 import { UsersRepository } from '../../../common/database/repository/users.repository.js';
 
 async function testCreateUserSuccess() {
+    let result = false;
+    let errorCalled = false;
     // 1) database 모킹
     const mockDatabase = {
         prepare: () => {
@@ -10,32 +12,43 @@ async function testCreateUserSuccess() {
     }
     // 2) Repository 생성 및 호출
     const usersRepository = new UsersRepository(mockDatabase);
-    const result = await usersRepository.createUser("name", "address");
-
+    try {
+        result = await usersRepository.createUser("name", "address");
+    } catch (err) {
+        errorCalled = true;
+    }
     // 3) 성공 시 값 대조
     assert.strictEqual(result, true, "성공 시 반환 값이 true 여야함");
+    assert.strictEqual(errorCalled, false, "성공 시 에러가 호출되지 않았어야함");
 
     console.log("testCreateUserSuccess: 통과")
 }
 
 async function testCreateUserFail() {
+    let result = false;
+    let errorCalled = false;
     // 1) database 모킹
     const mockDatabase = {
         prepare: () => {
-            return { run: async () => { return { changes: 0 } } }
+            return { run: async () => { throw new Error; } }
         }
     }
     // 2) Repository 생성 및 호출
-    const usersRepository = new UsersRepository(mockDatabase);
-    const result = await usersRepository.createUser("name", "address");
-
-    // 3) 성공 시 값 대조
+    try {
+        result = await usersRepository.createUser("name", "address");
+    } catch (err) {
+        errorCalled = true;
+    }
+    // 3) 실패 시 값 대조
     assert.strictEqual(result, false, "실패 시 반환 값이 false 여야함");
+    assert.strictEqual(errorCalled, true, "실패 시 에러가 호출되었어야함");
 
     console.log("testCreateUserFail: 통과")
 }
 
 async function testFindUserSuccess() {
+    let result = false;
+    let errorCalled = false;
     // 1) database 모킹
     const mockDatabase = {
         prepare: () => {
@@ -44,32 +57,46 @@ async function testFindUserSuccess() {
     }
     // 2) Repository 생성 및 호출
     const usersRepository = new UsersRepository(mockDatabase);
-    const result = await usersRepository.findUser("name");
-
+    try {
+        result = await usersRepository.findUser("name");
+    } catch (err) {
+        errorCalled = true;
+    }
+    
     // 3) 성공 시 값 대조
     assert.strictEqual(result, "result", "성공 시 반환 값이 result 이여야함");
+    assert.strictEqual(errorCalled, false, "성공 시 에러가 호출되지 않았어야함");
 
     console.log("testFindUserSuccess: 통과")
 }
 
 async function testFindUserFail() {
+    let result = false;
+    let errorCalled = false;
     // 1) database 모킹
     const mockDatabase = {
         prepare: () => {
-            return { get: async () => { return undefined  } }
+            return { get: async () => { throw new Error;  } }
         }
     }
     // 2) Repository 생성 및 호출
     const usersRepository = new UsersRepository(mockDatabase);
-    const result = await usersRepository.findUser("name");
+    try {
+        result = await usersRepository.findUser("name");
+    } catch (err) {
+        errorCalled = true;
+    }
 
     // 3) 실패 시 값 대조
-    assert.strictEqual(result, undefined, "실패 시 반환 값이 undefined 여야함");
+    assert.strictEqual(result, false, "실패 시 반환 값이 false 여야함");
+    assert.strictEqual(errorCalled, true, "실패 시 에러가 호출되었어야함");
 
     console.log("testFindUserFail: 통과")
 }
 
 async function testGetWinnerSuccess() {
+    let result = false;
+    let errorCalled = false;
     // 1) database 모킹
     const mockDatabase = {
         prepare: () => {
@@ -78,33 +105,47 @@ async function testGetWinnerSuccess() {
     }
     // 2) Repository 생성 및 호출
     const usersRepository = new UsersRepository(mockDatabase);
-    const result = await usersRepository.getWinner();
+    try {
+        result = await usersRepository.getWinner();
+    } catch (err) {
+        errorCalled = true;
+    }
 
     // 3) 성공 시 값 대조
     assert.strictEqual(result, "result", "성공 시 반환 값이 result 이여야함");
+    assert.strictEqual(errorCalled, false, "성공 시 에러가 호출되지 않았어야함");
 
     console.log("testGetWinnerSuccess: 통과")
 }
 
 async function testGetWinnerFail() {
+    let result = false;
+    let errorCalled = false;
     // 1) database 모킹
     const mockDatabase = {
         prepare: () => {
-            return { all: async () => { return undefined } }
+            return { all: async () => { throw new Error; } }
         }
     }
     // 2) Repository 생성 및 호출
     const usersRepository = new UsersRepository(mockDatabase);
-    const result = await usersRepository.getWinner();
+    try {
+        result = await usersRepository.getWinner();
+    } catch (err) {
+        errorCalled = true;
+    }
 
-    // 3) 성공 시 값 대조
-    assert.strictEqual(result, undefined, "실패 시 반환 값이 undefined 여야함");
+    // 3) 실패 시 값 대조
+    assert.strictEqual(result, false, "실패 시 반환 값이 false 여야함");
+    assert.strictEqual(errorCalled, true, "실패 시 에러가 호출되었어야함");
 
     console.log("testGetWinnerFail: 통과")
 }
 
 
 async function testUpdateCountSuccess() {
+    let result = false;
+    let errorCalled = false;
     // 1) database 모킹
     const mockDatabase = {
         prepare: () => {
@@ -113,27 +154,39 @@ async function testUpdateCountSuccess() {
     }
     // 2) Repository 생성 및 호출
     const usersRepository = new UsersRepository(mockDatabase);
-    const result = await usersRepository.updateCount(0, 0, 0);
+    try {
+        result = await usersRepository.updateCount(0, 0, 0);
+    } catch (err) {
+        errorCalled = true;
+    }
 
     // 3) 성공 시 값 대조
     assert.strictEqual(result, true, "성공 시 반환 값이 true 여야함");
+    assert.strictEqual(errorCalled, false, "성공 시 에러가 호출되지 않았어야함");
 
     console.log("testUpdateCountSuccess: 통과")
 }
 
 async function testUpdateCountFail() {
+    let result = false;
+    let errorCalled = false;
     // 1) database 모킹
     const mockDatabase = {
         prepare: () => {
-            return { run: async () => { return { changes: 0 } } }
+            return { run: async () => { throw new Error; } }
         }
     }
     // 2) Repository 생성 및 호출
     const usersRepository = new UsersRepository(mockDatabase);
-    const result = await usersRepository.updateCount(0, 0, 0);
+    try {
+        result = await usersRepository.updateCount(0, 0, 0);
+    } catch (err) {
+        errorCalled = true;
+    }
 
-    // 3) 성공 시 값 대조
+    // 3) 실패 시 값 대조
     assert.strictEqual(result, false, "실패 시 반환 값이 false 여야함");
+    assert.strictEqual(errorCalled, true, "실패 시 에러가 호출되었어야함");
 
     console.log("testUpdateCountFail: 통과")
 }
