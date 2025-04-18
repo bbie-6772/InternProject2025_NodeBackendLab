7b0767178bc82cb17fdabfe151b59f0263f8a0f2
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-04-19 07:11:52 +0900

    add: 클러스터 DB 작업용 Queue 구현

--- common/clustering/cluster.js
+++ common/clustering/cluster.js
@@ -3,14 +3,34 @@ import os from 'os';
 import { startEventServer } from '../../eventServer/server.js';
 import { startRegisterServer } from '../../registerServer/server.js';
 import { config } from '../config/config.js';
+import { createSchemas, db } from '../database/database.js';
+import { JobQueue } from '../utils/jobQueue.js';
+import { UsersRepository } from '../database/repository/users.repository.js';
 
 const numCPUs = os.cpus().length;  
 
-const startCluster = () => {
+const startCluster = async () => {
     if (cluster.isPrimary) {
         console.log(`대빵 Process ${process.pid} 작업중`);  
+        await createSchemas();
+        const jobQueue = new JobQueue();
+        const userRepository = new UsersRepository(db);
+
         for(let i = 0;i < numCPUs;i++) {
-            cluster.fork();
+            const worker = cluster.fork();
+
+            worker.on('message', async (msg) => {
+                const { id, method, args } = msg;
+                try {
+                    if (typeof userRepository[method] !== 'function')
+                        throw new Error(`Unknown method: ${method}`);
+
+                    const result = await jobQueue.enqueue(() => userRepository[method](...args));
+                    worker.send({ id, result });
+                } catch (err) {
+                    worker.send({ id, error: err.message });
+                }
+            }); 
         }
 
         cluster.on('exit', (worker, code, signal ) => {
---
+++ common/clustering/clusterQueue.js
@@ -0,0 +1,33 @@
+export class ClusterQueue {
+    constructor () {
+        this.nextRequestId = 0;
+        this.pendingRequests = new Map();  
+
+        // 응답 받는 이벤트 처리  
+        process.on('message', (msg) => {
+            const { id, result, error } = msg;
+            if (!this.pendingRequests.has(id)) return
+
+            const { resolve, reject } = this.pendingRequests.get(id);
+
+            if (error) {
+                reject(new Error(error));
+            } else {
+                resolve(result);
+            }
+            this.pendingRequests.delete(id);  
+        });
+    }
+
+    async sendRequestToMaster(msg) {
+        return new Promise((resolve, reject) => {
+            const id = this.nextRequestId++;
+            // 요청 ID 붙이고 보냄  
+            process.send({ id, ...msg });
+
+            // 응답을 받을 때까지 대기용 resolve 저장  
+            this.pendingRequests.set(id, { resolve , reject });
+        });
+    }
+}
+
