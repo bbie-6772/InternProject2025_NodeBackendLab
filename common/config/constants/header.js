2c7ff7d67f3c3bf33dfe44b3e8a97d7fe8ad8f91
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-04-19 07:13:18 +0900

    add: 로그인 성공 여부 응답 추가

--- common/config/constants/header.js
+++ common/config/constants/header.js
@@ -4,13 +4,13 @@ export const PAYLOAD_LENGTH_BYTE = 4;
 export const PACKET_TYPE = {
     // 첫 접속 
     C_LOGIN_REQUEST : 1001,
+    S_LOGIN_RESPONSE : 1002,
     
     // 클릭 이벤트 
     C_CLICK_REQUEST : 2001,
 
     // 우승자 확인 
     C_GET_WINNER_REQUEST : 3001,
-
     S_GET_WINNER_RESPONSE : 3002,
 
     // 에러
