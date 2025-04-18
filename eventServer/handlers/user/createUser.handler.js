654c7fde7f98cbb1d8971bdb601538852ded71dc
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-04-18 19:14:06 +0900

    fix: 작업 큐 메서드 적용

--- eventServer/handlers/user/createUser.handler.js
+++ eventServer/handlers/user/createUser.handler.js
@@ -1,18 +1,20 @@
 import { config } from "../../../common/config/config.js";
+import { onEnd } from "../../events/onEnd.js";
 import { userRepository, userSession } from "../../session.js"
 import { makePacket } from "../../utils/packet/makePacket.js";
 
 export const createUserHandler = async (socket, payload) => {
     const { name } = payload;
 
-    const id = userRepository.findUser(name);
-    if (!id) {
+    const results = await userRepository.enqueue(userRepository.findUser, name);
+    if (!results) {
         const response = { error: "User not found"};
         const packet = makePacket(config.header.packetType.S_ERROR_NOTIFICATION, response );
         socket.write(packet);
+        onEnd(socket)();
         return;
     }
 
-    socket.id = id;
+    socket.id = results.id;
     userSession.addUser(socket);
 }
--- registerServer/service/users.service.js
+++ registerServer/service/users.service.js
@@ -5,12 +5,13 @@ export class UsersService {
         this.userRepository = userRepository;
     }
  
-   createUser = async (req, res) => {
+    createUser = async (req, res) => {
         await parseJson(req, res);
         const { name, address } = req.body
-        if (!await this.userRepository.createUser(name, address))
-            throw new Error ("계정 생성 실패");
-       res.writeHead(200, { 'Content-Type': 'application/json' });
-       res.end(JSON.stringify({ results: 'Success' }));
+        if (!await this.userRepository.enqueue(this.userRepository.createUser, name, address))
+            throw new Error("계정 생성 실패");
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ results: 'Success' }));
     }
 }
