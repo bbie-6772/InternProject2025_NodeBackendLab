49ae7be6bb470ea35ea3748695031235cd9c18f5
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-04-18 13:10:07 +0900

    bugfix: db가 작동되지 않는 버그 수정
    SQLite 의 DB는 prepare()를 통해 반환되는 객체에서 run(실행) 할 수 있어,
    기존의 구조를 수정하였음

--- registerServer/repository/users.repository.js
+++ registerServer/repository/users.repository.js
@@ -1,4 +1,4 @@
-import { USERS_QUERIES } from "../database/queries.js"
+import { USERS_QUERIES } from "../../common/database/queries.js"
 
 export class UsersRepository {
     constructor(database) {
@@ -6,15 +6,8 @@ export class UsersRepository {
     }
 
     createUser = async (name, password) => {
-        const { changes } = await db.run(USERS_QUERIES.CREATE_USER, [name, password]);
-
+        const query = this.db.prepare(USERS_QUERIES.CREATE_USER);
+        const { changes } = await query.run(name, password);
         return changes > 0;
-    }
-
-    findUserByName = async (name) => {
-        const user = await db.get(USERS_QUERIES.FIND_USER, [name]);
-        console.log(user);
-
-        return user != undefined;
     }
 }
