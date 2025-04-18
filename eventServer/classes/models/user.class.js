260040691a4b20bc0dba64b80591fa2d9eea3027
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-04-18 19:13:18 +0900

    add: addCount 메서드 구현
    조건들에 부합할 때 클릭 수를 인정하도록 구현

--- eventServer/classes/models/user.class.js
+++ eventServer/classes/models/user.class.js
@@ -1,12 +1,38 @@
+import { userSession } from "../../session.js";
+
 export class User {
     constructor(socket) {
         this.socket = socket;
+        this.hasFailed =  false;
         this.lastCounts = [];
+        this.timer = null;
         this.clickCounts = 0;
         this.lastClick = null;
     }
 
     addCount() {
+        // console.log(`현재 카운트 수 ${this.clickCounts} / 실패 여부 ${this.hasFailed}`)
+        if(!userSession.isOpen || this.hasFailed ) return;
+        const now = new Date();
 
+        this.lastCounts.push(now);
+        if (this.lastCounts.length > 4) {
+            const delay = this.lastCounts[this.lastCounts.length-1] - this.lastCounts[0]
+            if (delay < 1000) {
+                // console.log(delay, "시간 너무 빠르다")
+                this.hasFailed = true;
+                return;
+            }
+                
+            this.lastCounts.shift();
+        }
+
+        this.clickCounts++;
+        this.lastClick = now.toISOString();
+
+        if (this.timer) {
+            clearTimeout(this.timer);
+        } 
+        this.timer = setTimeout(() => this.hasFailed = true, 10000)
     }
 }
