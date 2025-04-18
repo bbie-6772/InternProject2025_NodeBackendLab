d6b11f864cd94e1553334b77405e67247c8f1e7d
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-04-18 23:39:59 +0900

    fix: onData 의존성 분리

--- eventServer/events/onConnections.js
+++ eventServer/events/onConnections.js
@@ -1,12 +1,13 @@
 import { onEnd } from "./onEnd.js"
 import { onData } from "./onData.js"
 import { onError } from "./onError.js"
+import { handlers } from "../handlers/index.js"
 
 export const onConnection = async (socket) => {
     console.log("새로운 유저 접속", socket.remoteAddress, socket.remotePort);
     socket.buffer = Buffer.alloc(0);
 
-    socket.on('data', onData(socket));
+    socket.on('data', onData(socket, handlers, onEnd));
     socket.on('end', onEnd(socket));
     socket.on('error', onError(socket));
 }
--- eventServer/events/onData.js
+++ eventServer/events/onData.js
@@ -1,8 +1,6 @@
 import { config } from "../../common/config/config.js";
-import { handlers } from "../handlers/index.js";
-import { onEnd } from "./onEnd.js";
 
-export const onData = (socket) => async (data) => {
+export const onData = (socket, handlers, onEnd ) => async (data) => {
     socket.buffer = Buffer.concat([socket.buffer, data]);
     const packetTypeByte = config.header.packetTypeByte;
     const payloadLengthByte = config.header.payloadLengthByte;
@@ -11,23 +9,31 @@ export const onData = (socket) => async (data) => {
 
     try {
         while (socket.buffer.length >= defaultLength) {
-            try {
-                payloadByte = socket.buffer.readUInt32BE(packetTypeByte);
-            } catch (err) {
-                onEnd(socket)();
-                break;
-            }
-
+            payloadByte = socket.buffer.readUInt32BE(packetTypeByte);
             if (socket.buffer.length < defaultLength + payloadByte) break;
+
             const packet = socket.buffer.subarray(0, defaultLength + payloadByte);
             socket.buffer = socket.buffer.subarray(defaultLength + payloadByte);
 
             const packetType = packet.readUInt16BE(0);
             const payloadBuffer = packet.subarray(defaultLength, defaultLength + payloadByte);
             const payloadString = payloadBuffer.toString('utf8');
-            const payload = JSON.parse(payloadString);
+            let payload;
+            try {
+                payload = JSON.parse(payloadString);
+            } catch (err) {
+                console.error("JSON 파싱 실패:", err);
+                onEnd(socket)();
+                break;
+            }  
 
             const handler = handlers[packetType];
+            if (typeof handler !== 'function') {
+                console.error("올바르지 않은 패킷 타입");
+                onEnd(socket)();
+                break;
+            }
+                
             await handler( socket, payload );
         }
     } catch (err) {
