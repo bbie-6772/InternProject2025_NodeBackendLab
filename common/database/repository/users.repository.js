6f4b87654fbebd1bffb11933708b43c02812a622
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-04-18 19:11:16 +0900

    add: lock 회피를 위한 작업 큐 생성

--- common/database/repository/users.repository.js
+++ common/database/repository/users.repository.js
@@ -3,6 +3,38 @@ import { USERS_QUERIES } from "../queries.js"
 export class UsersRepository {
     constructor(database) {
         this.db = database;
+        this.jobQueue = [];
+        this.isQueueRunning = false;
+    }
+    
+    queueLoop = async () => {
+        while (this.jobQueue.length > 0) {
+            const job = this.jobQueue.shift();
+            try {
+                await job();
+            } catch (error) {
+                console.error('Queue task error:', error);
+            }
+        }
+        this.isQueueRunning = false;
+    }  
+
+    enqueue = (method, ...args) => {
+        return new Promise((resolve, reject) => {
+            this.jobQueue.push(async () => {
+                try {
+                    const result = await method(...args);
+                    resolve(result);
+                } catch (err) {
+                    reject(err);
+                }  
+            });
+
+            if (!this.isQueueRunning) {
+                this.isQueueRunning = true;
+                this.queueLoop();
+            }
+        });
     }
 
     createUser = async (name, address) => {
@@ -23,9 +55,9 @@ export class UsersRepository {
         return user;
     }
 
-    updateCount = async (id, count, lastClickTime) => {
+    updateCount = async (clickCount, lastClickTime, id) => {
         const query = this.db.prepare(USERS_QUERIES.UPDATE_COUNT);
-        const { changes } = await query.run(id, count, lastClickTime);
+        const { changes } = await query.run(clickCount, lastClickTime, id);
         return changes > 0;
     }
 }
