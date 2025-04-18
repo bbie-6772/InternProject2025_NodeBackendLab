62d5e983e9ed929af53833ff442a5367925cf30e
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-04-19 05:09:46 +0900

    fix: 필요 값만 조회하도록 수정

--- common/database/queries.js
+++ common/database/queries.js
@@ -2,5 +2,5 @@ export const USERS_QUERIES = {
     CREATE_USER: 'INSERT INTO users (name, address) VALUES (?,?)',
     UPDATE_COUNT: 'UPDATE users SET click_count = ?, last_click = ? WHERE id = ?',
     FIND_USER: 'SELECT id FROM users WHERE name = ?',
-    GET_WINNER: 'SELECT * FROM users WHERE click_count > 0 ORDER BY click_count DESC, last_click ASC LIMIT 1'
+    GET_WINNER: 'SELECT id, address, click_count FROM users WHERE click_count > 0 ORDER BY click_count DESC, last_click ASC LIMIT 1'
 }
